170266237e379add4e4df7daf19cb62633d068aa
Devin Gearing <devingearing@devingearingMAC.local>
2018-08-05 18:06:29 -0500

    removed clutter

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
-const devices = require('./routes/devices');
+//const devices = require('./routes/devices');
 const home = require('./routes/home');
-const uploads = require('./routes/uploads');
-const wemo = require('./routes/wemo');
-const manage = require('./routes/manage');
+//const uploads = require('./routes/uploads');
+//const wemo = require('./routes/wemo');
+//const manage = require('./routes/manage');
 const port = process.env.PORT || 3000;
-const connect = require('connect');
+
 const path = require('path');
 const fs = require('fs');
-const schedule = require('node-schedule');
+//const schedule = require('node-schedule');
 
 //This will run the scedular every X minutes
-const hitMinute = 1;
+//const hitMinute = 1;
 
 //var PythonShell = require('python-shell');
 
-var serveStatic = require('serve-static')
-const mustacheExpress = require('mustache-express');
-const mustache = require('mustache');
+//var serveStatic = require('serve-static')
+//const mustacheExpress = require('mustache-express');
+//const mustache = require('mustache');
 
 // const customers = require('./routes/customers');
 // const movies = require('./routes/movies');
 const express = require('express');
 const app = express();
 // app.use(express.static(__dirname + '/public'));
-app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
+//app.engine('mustache', mustacheExpress());
+//app.set('view engine', 'mustache');
 
 
 
@@ -63,35 +63,35 @@ app.set('view engine', 'mustache');
 
 //IF FOLDER uploads DOESNT EXIST THEN CREATE IT. THIS IS TO REDUCE SIZE WHILE ALLOWING GIT CONTROL
 // var fs = require('fs');
-var dir = './uploads';
-if (!fs.existsSync(dir)) {
+// var dir = './uploads';
+// if (!fs.existsSync(dir)) {
 
-  fs.mkdirSync(dir);
-}
-
-
-
-var j = schedule.scheduleJob(`*/${hitMinute} * * * *`, function () {
-  var unixTimeStamp = (new Date).getTime();
-  var timestampInMilliSeconds = unixTimeStamp;
-  var date = new Date(timestampInMilliSeconds);
-
-  var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
-  var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
-  var year = date.getFullYear();
-
-  //var hours = ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12);
-  var hours = date.getHours();
-  var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-  var meridiem = (date.getHours() >= 12) ? 'pm' : 'am';
-
-  var formattedDate = day + '-' + month + '-' + year + ' at ' + hours + ':' + minutes + ' ' ;
-  console.log(`THIS HITS EVERY ${hitMinute} MINUTES! - ` + formattedDate);
-
-  //JQUERY ALL POWER AND SAVE TO DATABASE
+//   fs.mkdirSync(dir);
+// }
 
 
-});
+
+// var j = schedule.scheduleJob(`*/${hitMinute} * * * *`, function () {
+//   var unixTimeStamp = (new Date).getTime();
+//   var timestampInMilliSeconds = unixTimeStamp;
+//   var date = new Date(timestampInMilliSeconds);
+
+//   var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+//   var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
+//   var year = date.getFullYear();
+
+//   //var hours = ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12);
+//   var hours = date.getHours();
+//   var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+//   var meridiem = (date.getHours() >= 12) ? 'pm' : 'am';
+
+//   var formattedDate = day + '-' + month + '-' + year + ' at ' + hours + ':' + minutes + ' ' ;
+//   console.log(`THIS HITS EVERY ${hitMinute} MINUTES! - ` + formattedDate);
+
+//   //JQUERY ALL POWER AND SAVE TO DATABASE
+
+
+// });
 
 //app.set('views', __dirname + '/views');
 
@@ -100,11 +100,11 @@ mongoose.connect('mongodb://localhost/wemo')
   .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
-app.use('/api/devices', devices);
-app.use('/uploads', uploads);
-app.use('/wemo', wemo);
+//app.use('/api/devices', devices);
+//app.use('/uploads', uploads);
+//app.use('/wemo', wemo);
 app.use('/', home);
-app.use('/manage', manage);
+//app.use('/manage', manage);
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 
