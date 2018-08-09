28f83e00d137ea8b06ff513cbc420a0659c5ee61
Devin Gearing <devingearing@devingearingMAC.local>
2018-08-09 16:15:58 -0500

    test change

--- public/js/main.js
+++ public/js/main.js
@@ -124,7 +124,23 @@ function typeEffect() {
         }
     };
 
-    window.onload = function () {
+    // window.onload = function () {
+    //     var elements = document.getElementsByClassName('typewrite');
+    //     for (var i = 0; i < elements.length; i++) {
+    //         var toRotate = elements[i].getAttribute('data-type');
+    //         var period = elements[i].getAttribute('data-period');
+    //         if (toRotate) {
+    //             new TxtType(elements[i], JSON.parse(toRotate), period);
+    //         }
+    //     }
+    //     // INJECT CSS
+    //     var css = document.createElement("style");
+    //     css.type = "text/css";
+    //     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    //     document.body.appendChild(css);
+    // };
+
+    function startTypeWrite() {
         var elements = document.getElementsByClassName('typewrite');
         for (var i = 0; i < elements.length; i++) {
             var toRotate = elements[i].getAttribute('data-type');
@@ -140,6 +156,7 @@ function typeEffect() {
         document.body.appendChild(css);
     };
 
+    startTypeWrite();
 }
 
 
